080b043d18ec423dd828e1673bb28cd2ce0d4db4
nichoth <nichoth@nichoth.com>
2026-07-02 14:05:29 -0700

    setup

--- example/index.ts
+++
@@ -1,8 +0,0 @@
-import { type FunctionComponent, render } from 'preact'
-import { html } from 'htm/preact'
-
-const Example:FunctionComponent<unknown> = function () {
-    return html`<div>hello</div>`
-}
-
-render(html`<${Example} />`, document.getElementById('root')!)
