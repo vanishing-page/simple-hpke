f1707648b310c03e6d76b03dfce28773a7408e0d
nichoth <nichoth@nichoth.com>
2026-07-02 01:42:31 -0700

    test: add Node test runner and cipher-suite smoke test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
 import { test } from '@substrate-system/tapzero'
-import { example } from '../src/index.js'
+import { suite } from '../src/index.js'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
-    example()
+test('cipher suite is configured', async t => {
+    t.equal(suite.KEM.Nenc, 32, 'X25519 encapsulated key is 32 bytes')
 })
